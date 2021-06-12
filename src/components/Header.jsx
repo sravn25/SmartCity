3e481953277f1559d856cb4243b0307683dd30d8
sravn25 <victor.sravn24@gmail.com>
2021-06-12 13:33:37 +0800

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import Navbar from "./Navbar";
+import "./HeaderStyles.css";
+import Logo from "../images/logo.png";
+import Name from "../images/transparentName.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg">
+      <Container fluid>
+        <Row>
+          <Col xs>
+            <div className="header">
+              <img src={Logo} alt="logo" width="100px" height="100px" />
+              <img src={Name} alt="name" width="480px" height="100px" />
+            </div>
+          </Col>
+          <Col xs={2} md={1}>
+            <div className="navbar">
+              <Navbar />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
