89b7b7a650cb15422d224b190971bbde82ea89c2
sravn25 <victor.sravn24@gmail.com>
2021-06-13 08:49:14 +0800

    updated index layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollButton from "./components/ScrollButton";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
+    <ScrollButton />
     <App />
+    <Footer />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
