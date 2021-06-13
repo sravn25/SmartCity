8dff6cd786279e5b3e1d55b3008c838cd8e2412a
sravn25 <victor.sravn24@gmail.com>
2021-06-13 09:32:45 +0800

    added routers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Commute from "./pages/Commute";
+import News from "./pages/News";
+import Events from "./pages/Events";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path="/commute">
+            <Commute />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path="/news">
+            <News />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route path="/events">
+            <Events />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
